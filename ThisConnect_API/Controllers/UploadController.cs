113925caaa341f1342f27190c9df120fa97a140d
metinoztay <metinoztay@gmail.com>
2024-09-15 16:16:14 +0300

    Photo Resizer Added

--- ThisConnect_API/Controllers/UploadController.cs
+++ ThisConnect_API/Controllers/UploadController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SixLabors.ImageSharp.Formats.Jpeg;
 using ThisConnect_API.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace ThisConnect_API.Controllers
 {
@@ -21,34 +25,48 @@ namespace ThisConnect_API.Controllers
         #region User Photo Upload
         [HttpPost("UploadProfilePhoto/{userId}")]
         public async Task<IActionResult> UploadProfilePhoto(string userId, [FromForm] IFormFile file)
-        {           
-
+        {
             if (file == null || file.Length == 0)
                 return BadRequest("No file uploaded");
 
             var uploadsFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "Photos", "UserProfilePhotos");
 
-            // Klasör var mı kontrol et ve oluştur
             if (!Directory.Exists(uploadsFolderPath))
             {
                 Directory.CreateDirectory(uploadsFolderPath);
             }
-            var user = await _context.Users.FindAsync(userId);
-            string fileName = file.FileName;
-            string extension = Path.GetExtension(fileName);
-            var filePath = Path.Combine(uploadsFolderPath, user.UserId.ToString() + extension);
 
-            using (var stream = new FileStream(filePath, FileMode.Create))
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+                return NotFound("User not found");
+
+            string extension = Path.GetExtension(file.FileName);
+            string fileName = $"{user.UserId}{extension}";
+            var filePath = Path.Combine(uploadsFolderPath, fileName);
+
+            
+            using (var image = Image.Load(file.OpenReadStream()))
             {
-                await file.CopyToAsync(stream);
+                
+                image.Mutate(x => x.Resize(new ResizeOptions
+                {
+                    Mode = ResizeMode.Max,
+                    Size = new Size(300, 300) 
+                }));
+
+                
+                var encoder = new JpegEncoder
+                {
+                    Quality = 75 
+                };
+
+                await image.SaveAsync(filePath, encoder);
             }
 
+            var fileUrl = $"{Request.Scheme}://{Request.Host}/Photos/UserProfilePhotos/{fileName}";
 
-            var fileUrl = $"{Request.Scheme}://{Request.Host}/Photos/UserProfilePhotos/{user.UserId + extension}";
-
-
+            
             user.AvatarUrl = fileUrl;
-
             _context.Entry(user).State = EntityState.Modified;
             await _context.SaveChangesAsync();
 
